c8411e6b7dc4fd8adbffe40a66bb160698804161
Btaft6278 <btaft6278@gmail.com>
2022-06-12 20:00:18 -0400

    eod

--- pages/bubble.js
+++ pages/bubble.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import Method from './method';
 import randomArray from "./randomArray";
 import { ButtonBox, SortButton, RefreshButton, ArraySizeButton } from '../src/components/MyButtons.js';
@@ -8,15 +8,13 @@ import styles from '../src/css/bubble.module.css';
 
 function Bubble() {
     const [speed, setSpeed] = useState(1000);
-    const [text, setText] = useState([]);
+    const [capText, setCapText] = useState("");
     const [textTwo, setTextTwo] = useState([]);
     const [bottomText, setBottomText] = useState([]);
     const [bottomTextTwo, setBottomTextTwo] = useState([])
     const { newRandomArray, setNewRandomArray, refresh, setRefresh, max, setMax } = randomArray();
+    const arri = useRef();
 
-
-
-    // style={{ height: `${bar * 10}%`}}
     async function bubbleSort() {
 
         const arr = newRandomArray;
@@ -26,16 +24,15 @@ function Bubble() {
             checked = false
             for (let i = 0; i < arr.length; i++) {
                 await new Promise(resolve => setTimeout(resolve, speed));
-                console.log(speed)
-                //The element we are testing turns red
-                //document.getElementById(i).style.backgroundColor = "red";
+                document.getElementById(i).classList.toggle("selected-color");
+                document.getElementById(i).innerText = "arr[i]";
                 // document.getElementById(`caption${i}`).innerText = "<<<=== arr[i]"
-                if (document.getElementById(i + 1) !== null) {
-                    //document.getElementById(i + 1).style.backgroundColor = "white";
-                    // document.getElementById(`caption${i + 1}`).innerText = "<<<=== arr[i + 1]";
-                }
+                // if (document.getElementById(i + 1) !== null) {
+                //     //document.getElementById(i + 1).style.backgroundColor = "white";
+                //     // document.getElementById(`caption${i + 1}`).innerText = "<<<=== arr[i + 1]";
+                // }
                 if (arr[i] > arr[i + 1]) {
-                    setText("If arr[i] is greater than arr[i + 1]")
+                    // setText("If arr[i] is greater than arr[i + 1]")
                     await new Promise(resolve => setTimeout(resolve, speed));
                     setBottomText("They swap positions and the loop moves to the next element")
                     await new Promise(resolve => setTimeout(resolve, speed));
@@ -57,7 +54,7 @@ function Bubble() {
                     setBottomTextTwo("They stay where they are and the loop continues to next element");
                     await new Promise(resolve => setTimeout(resolve, speed));
                 }
-                // document.getElementById(i).style.backgroundColor = "#89DDff";
+                document.getElementById(i).classList.toggle("selected-color");
                 // document.getElementById(`caption${i}`).innerText = "";
 
                 if (document.getElementById(i + 1) !== null) {
@@ -75,33 +72,19 @@ function Bubble() {
             //Ensuring loop will not run on a sorted array more than once
         } while (checked)
     }
-
-    // const display = newRandomArray.map((bar, index) => {
-    //     return (
-    //         <div className={styles.bar} key={index} id={`${index}`} >
-    //             <i className={styles.cap}></i>
-
-    //             <span className={styles.test} style={newRandomArray.length < 10 && bar > 3 ? {height: `${bar * 15}%`} : {height: `${(bar + 7) * 5}%`} }></span>
-    //             <i className={styles.fill} ></i>
-
-    //             <div className={`${styles.cap} ${styles.capBottom}`}><div>{bar}</div></div>
-    //         </div>
-    //         // <span id={`caption${index}`}></span>
-    //     )
-    // });
-
+    // <span id={`caption${index}`}></span>
     const display = newRandomArray.map((bar, index) => {
+        let cssProperties = {'--percent': `${bar * (100 / newRandomArray.length)}`}
         return (
-            <div className="tube" data-percent="50" key={index} id={`${index}`} >
-        <i className="cap"></i><i className="fill"></i>
-        <div className="base">
-            <div className="text">{bar}</div>
-            <div className="icon">Base</div>
-        </div>
-    </div>
+            <div className="tube" style={cssProperties} key={index} id={`${index}`} >
+                <i className="cap"><span className='cap-text'>arr[i]</span></i><i className="fill" key={bar}></i>
+                <div className="base">
+                    <div className="icon text">{bar}</div>
+                </div>
+            </div>
         )
     });
-    
+
     return (
         <div>
 
@@ -110,14 +93,14 @@ function Bubble() {
                 <Method method={"bubble"} />
             </Box>
             <Box className={styles.newArray}>
-                        [{newRandomArray.toString()}]
-                    </Box>
+                [{newRandomArray.toString()}]
+            </Box>
             <div className={styles.row}>
                 {display}
             </div>
-            
-  
-       
+
+
+
         </div>
     )
 }
