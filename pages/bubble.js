d4e82b6317de2f43630bea243c99e4a61457ac5f
Btaft6278 <btaft6278@gmail.com>
2022-06-18 20:25:51 -0400

    eod

--- pages/bubble.js
+++ pages/bubble.js
@@ -1,23 +1,21 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import Highlighter from "react-highlight-words";
-import Method from './method';
+import Method from "./method";
 import randomArray from "./randomArray";
-import { ButtonBox } from '../src/components/MyButtons.js';
-import Box from '@mui/material/Box';
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
-import CompareArrowsIcon from '@mui/icons-material/CompareArrows';
-import styles from '../src/css/bubble.module.css';
+import { ButtonBox } from "../src/components/MyButtons.js";
+import Box from "@mui/material/Box";
+import styles from "../src/css/bubble.module.css";
 
 function Bubble() {
     const [speed, setSpeed] = useState(1000);
     const [text, setText] = useState("");
+    const [checked, setChecked] = useState("");
     const [explanation, setExplanation] = useState("")
     const { newRandomArray, setNewRandomArray, refresh, setRefresh, max, setMax } = randomArray();
 
     useEffect(() => {
         setExplanation(`The block of code containing the for loop will run through the array,checking if the current element is greater than the next,
-        if it is then it will swap the places of those two elements and sets checked to true. Once it finishes running, if a change was mae and checked is true
+        if it is then it will swap the places of those two elements and sets checked to true. Once it finishes running, if a change was made and checked is true
         it will pass the condition set in the while statement so the block of code will run again. Starting by setting checked to false, so once the for loop 
         runs through the whole array without making a change, checked will still be false and will not pass the condition in the while statement.
         This will then break out of the code.
@@ -31,7 +29,9 @@ function Bubble() {
 
         do {
             checked = false
+            setChecked("False")
             for (let i = 0; i < arr.length; i++) {
+                console.log(i,arr[i],arr[i+1])
                 await new Promise(resolve => setTimeout(resolve, speed));
                 // document.getElementById(i).classList.toggle("test");
                 document.getElementById(i).classList.toggle("current-element");
@@ -42,7 +42,6 @@ function Bubble() {
                 }
 
                 if (arr[i] > arr[i + 1]) {
-                    setExplanation("")
                     await new Promise(resolve => setTimeout(resolve, speed));
                     setText("Since arr[i] is greater than arr[i + 1] so they swap positions and the loop moves to the next element")
                     await new Promise(resolve => setTimeout(resolve, speed));
@@ -50,15 +49,12 @@ function Bubble() {
                     let tmp = arr[i];
                     arr[i] = arr[i + 1];
                     arr[i + 1] = tmp;
-
                     //Is only changed to true when there is a swap made
-
                     checked = true
-
+                    setChecked("True")
                 } else {
                     setText("Since arr[i] is less than arr[i + 1] They stay where they are and the loop continues to next element");
                     await new Promise(resolve => setTimeout(resolve, speed));
-
                 }
                 document.getElementById(i).classList.toggle("current-element");
                 document.getElementById(`cap${i}`).classList.toggle("current-element-text");
@@ -66,6 +62,8 @@ function Bubble() {
                     document.getElementById(i + 1).classList.toggle("next-element");
                     document.getElementById(`cap${i + 1}`).classList.toggle("next-element-text");
                 }
+
+
                 setText("");
                 setNewRandomArray([...arr]);
                 await new Promise(resolve => setTimeout(resolve, 500));
@@ -77,38 +75,35 @@ function Bubble() {
 
 
     const display = newRandomArray.map((bar, index) => {
-        let cssProperties = { '--percent': `${bar * (100 / newRandomArray.length)}` }
+        let cssProperties = { "--percent": `${bar * (100 / newRandomArray.length)}`, "--duration": `${speed / 1200}s` }
         return (
-            <>
-            <div className="tube" style={cssProperties} key={bar} id={`${index}`} >
-                <i className="cap"><span className="cap-text" id={`cap${index}`}></span></i><i className="fill" key={bar}></i>
-                <div className="base">
-                    <div className="text">{bar}</div>
-                </div>
-            </div>
-            <i class="arrow left"></i>
-            </>
+                <div className="tube" style={cssProperties} key={bar} id={`${index}`} >
+                    <i className="cap" id={`cap${index}`}></i><i className="fill" key={bar}></i>
+                    <div className="base">
+                        <div className="text">{bar}</div>
+                    </div>
+                </div>     
         )
     });
 
     return (
         <div>
-            <Box className='top-container'>
-            <ButtonBox
-                newRandomArray={newRandomArray}
-                sortMethod={bubbleSort}
-                refresh={refresh}
-                setRefresh={setRefresh}
-                setMax={setMax}
-                speed={speed}
-                setSpeed={setSpeed}
-            />
-                
-            
+            <Box className="top-container">
+                <ButtonBox
+                    newRandomArray={newRandomArray}
+                    sortMethod={bubbleSort}
+                    refresh={refresh}
+                    setRefresh={setRefresh}
+                    setMax={setMax}
+                    speed={speed}
+                    setSpeed={setSpeed}
+                />
+
+
                 <Method method={"bubble"} />
             </Box>
-            
-            
+
+
             <Box className="thought-bubble-container">
                 <Box className="thought-bubble">
                     <Highlighter
@@ -119,10 +114,11 @@ function Bubble() {
                     />
                 </Box>
                 <Box className="pointer"></Box>
+                <Box className={"var-container"}>
+               <span className={"array-span"}>Array = [{newRandomArray.toString()}]</span>  <span className={"checked-span"}>Checked = {checked}</span>
             </Box>
-        
-            
-                {/* [{newRandomArray.toString()}] */}
+            </Box>
+
             
             <div className={styles.row}>
                 {display}
