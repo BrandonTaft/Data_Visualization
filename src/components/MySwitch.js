d3dbcbeed38ef2b26a9e49dc036230b8730d79a8
Btaft6278 <btaft6278@gmail.com>
2022-07-09 20:10:54 -0400

    eod

--- src/components/MySwitch.js
+++ src/components/MySwitch.js
@@ -7,7 +7,7 @@ const Switch = ({ handleChange }) => {
     <Image src="/icons/night-mode.png" alt="github" layout="intrinsic" width={30} height={30} />
     <label className="switch">
       <input type="checkbox" onChange={handleChange} />
-      <span className="slider round"></span>
+      <span className="switcher round"></span>
     </label>
     <Image src="/icons/brightness.png" alt="github" layout="intrinsic" width={30} height={30} />
     </div>
